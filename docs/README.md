7862952eeddf5cc18dcb8a0fbd6455d6fbb74666
Nicolás López <nicolaslopezj@me.com>
2016-12-26 16:49:57 -0300

    initial

---
+++ docs/README.md
@@ -0,0 +1,14 @@
+## Decorators
+
+### debounce
+
+Props change will hang until changes stop in a period of specified time.
+
+#### Example
+
+```js
+@debounce(500)
+export default class Component extends React.Component {
+
+}
+```
---
+++ src/decorators/debounce.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import debounce from 'lodash/debounce'
+
+export default function (debounceTime) {
+  return function (Child) {
+    return class Debounce extends React.Component {
+
+      static propTypes = {
+        variables: React.PropTypes.object
+      }
+
+      constructor (props) {
+        super(props)
+        this.deboucedDidRecieveProps = debounce(this.didRecieveProps, debounceTime)
+        this.state = {props, propsIndex: 1}
+      }
+
+      didRecieveProps (props) {
+        this.setState(oldState => {
+          return {props, debouncing: false, propsIndex: oldState.propsIndex + 1}
+        })
+      }
+
+      componentWillReceiveProps (nextProps) {
+        this.setState({debouncing: true})
+        this.deboucedDidRecieveProps(nextProps)
+      }
+
+      shouldComponentUpdate (nextProps, nextState) {
+        if (nextState.propsIndex !== this.state.propsIndex) return true
+        if (nextState.debouncing !== this.state.debouncing) return true
+        return false
+      }
+
+      render () {
+        console.log('rerendering', this.state.propsIndex)
+        return <Child ref='child' debouncing={this.state.debouncing} {...this.state.props} />
+      }
+
+    }
+  }
+}
