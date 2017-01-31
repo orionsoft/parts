8938630bb0be3ef70d7d8665158822f5d9b85699
Nicolás López <nicolaslopezj@me.com>
2017-01-31 16:03:59 -0300

    text area y cancel events when in textarea

---
+++ src/components/fields/Textarea/styles.js
@@ -0,0 +1,40 @@
+import reactCSS from 'reactcss'
+
+export default reactCSS({
+  'default': {
+    container: {
+      marginBottom: 10,
+      display: 'flex',
+      width: '100%',
+      boxSizing: 'border-box',
+      fontSize: '20px',
+      fontFamily: 'inherit',
+      border: 'none',
+      borderRadius: '5px',
+      backgroundColor: '#eee'
+    },
+    input: {
+      flex: 1,
+      boxSizing: 'border-box',
+      fontSize: '20px',
+      fontFamily: 'inherit',
+      border: 'none',
+      borderRadius: '5px',
+      backgroundColor: '#eee',
+      padding: '10px',
+      outline: 'none'
+    },
+    otherContainer: {
+      textAlign: 'right',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 10
+    },
+    error: {
+      color: '#ff3030',
+      marginTop: 10,
+      marginBottom: 10
+    }
+  }
+})
