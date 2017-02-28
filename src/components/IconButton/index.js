a049669699f5874a0d1f7d634ca14f462024cc24
Nicolás López <nicolaslopezj@me.com>
2017-02-28 03:11:21 -0300

    improvements on icon button and select

--- src/components/IconButton/index.js
+++ src/components/IconButton/index.js
@@ -1,24 +1,54 @@
 import React from 'react'
+import Tooltip from '../Tooltip'
+import autobind from 'autobind-decorator'
 
 export default class IconButton extends React.Component {
 
   static propTypes = {
     icon: React.PropTypes.func,
     size: React.PropTypes.number,
-    onPress: React.PropTypes.func
+    onPress: React.PropTypes.func,
+    style: React.PropTypes.object,
+    tooltip: React.PropTypes.string,
+    disabled: React.PropTypes.bool,
+    loading: React.PropTypes.bool
   }
 
   static defaultProps = {
     size: 24,
-    onPress: () => {}
+    onPress: () => {},
+    style: {}
   }
 
-  render () {
+  @autobind
+  onPress () {
+    if (this.props.disabled) return
+    if (this.props.loading) return
+    this.props.onPress()
+  }
+
+  getClassName () {
+    const classes = []
+    if (this.props.disabled) classes.push('os_iconButton_disabled')
+    if (this.props.loading) classes.push('os_iconButton_loading')
+    if (!this.props.disabled && !this.props.loading) classes.push('os_iconButton')
+    return classes.join(' ')
+  }
+
+  renderButton () {
     return (
-      <span className='os_iconButton' onClick={this.props.onPress}>
+      <span className={this.getClassName()} onClick={this.onPress} style={this.props.style}>
         <this.props.icon size={this.props.size} />
       </span>
     )
+  }
+
+  render () {
+    if (this.props.tooltip) {
+      return <Tooltip content={this.props.tooltip}>{this.renderButton()}</Tooltip>
+    } else {
+      return this.renderButton()
+    }
   }
 
 }
