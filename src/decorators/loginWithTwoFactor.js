6b9aa6f7af72ccaa0cdd3c915e2d5650bafcd5a2
Nicolás López <nicolaslopezj@me.com>
2017-03-06 17:16:50 -0300

    two factor helper

---
+++ src/decorators/loginWithTwoFactor.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import withMutation from 'react-apollo-decorators/lib/withMutation'
+import gql from 'graphql-tag'
+import autobind from 'autobind-decorator'
+import hashPassword from '../helpers/hashPassword'
+
+export default function (ComposedComponent) {
+  @withMutation(gql`mutation login ($email: String, $password: HashedPassword, $code: String) {
+    loginWithTwoFactor (email: $email, password: $password, code: $code) {
+      id
+      token
+      tokenExpires
+    }
+  }`)
+  class LoginWithTwoFactor extends React.Component {
+
+    static propTypes = {
+      login: React.PropTypes.func
+    }
+
+    @autobind
+    async login ({email, password, code}) {
+      const {loginWithTwoFactor} = await this.props.login({
+        email,
+        password: hashPassword(password),
+        code
+      })
+      return loginWithTwoFactor
+    }
+
+    render () {
+      return (
+        <div>
+          <ComposedComponent loginWithTwoFactor={this.login} {...this.props} />
+        </div>
+      )
+    }
+  }
+
+  return LoginWithTwoFactor
+}
