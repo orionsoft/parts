518f80a12f9fcad0a6302432c55ce244fa0f6ef3
Nicolás López <nicolaslopezj@me.com>
2017-04-26 01:54:55 -0300

    improve file url

--- src/components/fields/File/cleanFileURL.js
+++ src/components/fields/File/cleanFileURL.js
@@ -1,7 +1,7 @@
 export default function (url) {
   const parts = url.split('/')
   const last = parts[parts.length - 1]
-  const withOutId = last.replace(/^\w+_/g, '')
+  const withOutId = last.replace(/^\w+?(_)/g, '')
   const clean = decodeURIComponent(withOutId)
   return clean
 }
