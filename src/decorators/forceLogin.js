30c99d1e91365b7dd730ad2617753a1abcf9610e
Bastian Ermann <bastian@ermannb.com>
2018-02-19 20:31:29 -0300

    Fix forceLogin decorator for react-router-v4

--- src/decorators/forceLogin.js
+++ src/decorators/forceLogin.js
@@ -10,7 +10,7 @@ export default function (ComposedComponent) {
     }
 
     redirect () {
-      this.context.router.replace({
+      this.context.router.history.replace({
         pathname: '/login',
         state: { nextPathname: window.location.pathname }
       })
