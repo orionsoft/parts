dac76f9ce14db05de0ba8179cf86f1716c7ccfda
Bastian Ermann <bastian@ermannb.com>
2018-02-19 11:10:50 -0300

    Update to react-router v4

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router'
+import {Link} from 'react-router-dom'
 import withoutLink from './withoutLink'
 
 export default class Button extends withoutLink {
