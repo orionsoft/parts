29f5fdaf74a494a70cc86b99b794a427eef1b570
Nicolás López <nicolaslopezj@me.com>
2018-10-27 21:11:50 -0300

    convert container to css & use safe inset area

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,24 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const styles = {
-  base: {
-    padding: 20,
-    paddingTop: 0,
-    paddingBottom: 0,
-    margin: '0 auto'
-  },
-  big: {
-    maxWidth: 1200
-  },
-  small: {
-    maxWidth: 600
-  },
-  medium: {
-    maxWidth: 900
-  }
-}
-
 export default class Container extends React.Component {
   static propTypes = {
     children: PropTypes.any,
@@ -27,18 +9,22 @@ export default class Container extends React.Component {
   }
 
   static defaultProps = {
-    size: 'big'
+    size: 'big',
+    style: {}
   }
 
-  getStyle() {
-    return {
-      ...styles.base,
-      ...styles[this.props.size],
-      ...this.props.style
-    }
+  getClassName() {
+    const className = ['os_container']
+    className.push(`os_container_${this.props.size}`)
+
+    return className.join(' ')
   }
 
   render() {
-    return <div style={this.getStyle()}>{this.props.children}</div>
+    return (
+      <div className="os_container" style={this.props.style}>
+        {this.props.children}
+      </div>
+    )
   }
 }
