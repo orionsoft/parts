ac23c6bbbef255176ed636cee66e48e2b95dfd98
Nicolás López <nicolaslopezj@me.com>
2018-11-07 06:47:11 -0300

    agregar custom classNAme

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -5,17 +5,22 @@ export default class Container extends React.Component {
   static propTypes = {
     children: PropTypes.any,
     style: PropTypes.object,
-    size: PropTypes.string
+    size: PropTypes.string,
+    className: PropTypes.string
   }
 
   static defaultProps = {
     size: 'big',
-    style: {}
+    style: {},
+    className: ''
   }
 
   getClassName() {
     const className = ['os_container']
     className.push(`os_container_${this.props.size}`)
+    if (this.props.className) {
+      className.push(this.props.className)
+    }
 
     return className.join(' ')
   }
